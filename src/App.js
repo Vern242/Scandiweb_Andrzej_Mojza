9130f98a84e217a48534f2a3997d22957e5bd232
Andrzej Mojza <alp423@gmail.com>
2022-10-29 20:00:01 +0200

    create_react_app+classes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Category from "./Category/Category";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Category />
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
