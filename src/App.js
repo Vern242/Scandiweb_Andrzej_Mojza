d0131a37c37da4371f18415443bff7bad8bbd11e
Andrzej Mojza <alp423@gmail.com>
2022-11-04 01:04:33 +0100

    add_categories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
+import React from "react";
 import Category from "./Category/Category";
+import { client, Query } from "@tilework/opus";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-      <Category />
-      <footer></footer>
-    </div>
-  );
+client.setEndpoint("http://localhost:4000/graphql");
+
+class App extends React.Component {
+  componentDidMount() {
+    console.log("mounted");
+    this.fetchCategories();
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { categories: [] };
+  }
+
+  async fetchCategories() {
+    const query = new Query("categories", true);
+    query.addField("name");
+    const result = await client.post(query).then((res) => res.categories);
+    this.setState({ categories: result });
+    console.log(result);
+    return result;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div> NAVBAR HERE</div>
+        <nav>
+          <ul>
+            {this.state.categories.map((category, index) => {
+              return (
+                <li key={`category${index}`}>
+                  <Link to={`/${category.name}`}>{category.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <Routes>
+          {this.state.categories.map((category) => {
+            return <Route key={`route_${category}`} element={<Category category={category.name} />} path={`/${category.name}`} />;
+          })}
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/Category/Category.js
+++ src/Category/Category.js
@@ -1,13 +1,47 @@
 import React from "react";
+import { client, Field, Query } from "@tilework/opus";
+
+client.setEndpoint("http://localhost:4000/graphql");
 
 class Category extends React.Component {
-  componentDidMount() {}
-  componentWillUnmount() {}
-  constructor(props) {
-    super();
+  componentDidMount() {
+    console.log("component mounted: " + this.props.category);
+    this.fetchProducts();
   }
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) this.fetchProducts();
+  }
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  async fetchProducts() {
+    const { category } = this.props;
+    console.log(category);
+
+    const query = new Query("category", true);
+    query.addField("name");
+    query.addField(new Field("products").addField("id").addField("name"));
+
+    query.addArgument("input", "CategoryInput", { title: category });
+
+    const result = await client.post(query).then((res) => res.category.products);
+    this.setState({ products: result });
+    console.log(result);
+    return result;
+  }
+
+  //output cat name and category items styled
   render() {
-    return <h1>Category</h1>;
+    return (
+      <div>
+        <h1>{this.props.category}</h1>
+        {this.state.products.map((prod) => {
+          return <div key={prod.id}>{prod.name}</div>;
+        })}
+      </div>
+    );
   }
 }
 
